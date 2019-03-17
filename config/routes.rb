e6ed63d5f93d0b1aac20273bb4f2c633d3238a0a
sheetal.sompura1985@gmail.com <sheetal.sompura1985@gmail.com>
2019-03-17 20:06:28 +0000

    lesson 36:added modal for lesson and section

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
-      resources :lessons, only: [:update]
-      resources :sections, only: [:update] do
-      resources :lessons, only: [:new, :create]
+    resources :lessons, only: [:update]
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
