43a1157a00acf938c1596679f927bd0f234e0ebb
sheetal.sompura1985@gmail.com <sheetal.sompura1985@gmail.com>
2019-03-19 06:31:17 +0000

    finished lesson 37adding next lesson button and resolved the video uploader issue

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,8 +1,8 @@
 class Section < ApplicationRecord
-  belongs_to :course
-  has_many :lessons
+    belongs_to :course 
+    has_many :lessons
 
-  include RankedModel
+    include RankedModel
     ranks :row_order, with_same: :course_id
     
     def next_section
